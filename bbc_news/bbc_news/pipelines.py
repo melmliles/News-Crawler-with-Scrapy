cc55a8b62e59cb7b8b850cc1088c378d934441a0
Mustapha EL MLILES <elmliles.mustafa@gmail.com>
2020-05-05 18:13:59 +0000

    Store the data in a hosted Mongo database
    Because:
     - Databases are efficients
     - Many person can access the same database
    
    I stored the items extracted from the bbc website into the MongoDB.

--- bbc_news/bbc_news/pipelines.py
+++ bbc_news/bbc_news/pipelines.py
@@ -4,13 +4,24 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+import pymongo
 
 class BbcNewsPipeline:
+
+    mongo_uri = "mongodb+srv://mustapha:mustapha123456@mycluster-dyvng.mongodb.net/test?retryWrites=true&w=majority"
+    mongo_db = "BBC"
+    collection_name = "bbc_articles"
+    
+    def __init__(self):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        db = self.client[self.mongo_db]
+        self.collection = db[self.collection_name]
+
     def process_item(self, item, spider):
         # How to add 'http://www.bbc.com' to incomplete articles URL
         for i in range(0,len(item['article_url'])):
             if item['article_url'][i][0:4] != 'http':
                 item['article_url'][i] = 'http://www.bbc.com' + item['article_url'][i]
-
+            
+        self.collection.insert(dict(item))
         return item
